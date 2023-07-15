7dc0bd2553672d31161983ce284f939f6f112492
unknown <dheanssler@gmail.com>
2023-07-15 18:50:29 -0400

    Initial Commit

---
+++ PingSweep.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "This scanner currently works with /24 networks only. Enter the first three octets of the network."
+echo "Example: 10.10.10"
+read subnet
+octets="$(echo $subnet | cut --output-delimiter=" " -d "." -f 1-4 | wc -w)"
+echo $octets
+if [ $octets -eq "3" ]; then
+	for i in {1..254}
+	do
+		ping -c 3 "$subnet"".""$i" 2>/dev/null | grep "ttl" | grep -E -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | uniq &
+		sleep 5
+	done
+elif [ $octets -eq "2" ]; then
+	for i in {1..254}
+	do
+		echo "Scanning subnet" $subnet"."$i".0/24"
+		for x in {1..254}
+		do
+			ping -c 3 "$subnet"".""$i"".""$x" 2>/dev/null | grep "ttl" | grep -E -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | uniq &	
+		done
+		sleep 5
+	done
+elif [ $octets -eq "1" ]; then
+	for i in {1..254}
+	do
+		for x in {1..254}
+		do
+			echo "Scanning subnet" $subnet"."$i"."$x".0/24"
+			for y in {1..254}
+			do
+				ping -c 3 "$subnet"".""$i"".""$x"".""$y" 2>/dev/null | grep "ttl" | grep -E -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | uniq &
+			done
+			sleep 5
+		done
+		sleep 5
+	done
+fi
+
+
+while [ "$(jobs -r | wc -l)" -gt 0 ]
+do
+	sleep 5
+	echo "Jobs still running..."
+done
+
+echo "Done."
